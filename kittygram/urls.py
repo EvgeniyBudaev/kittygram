63d0c35ff575c458a7b8acbefc8a7ea883c3d8de
Evgeniy Budaev <budaev.e@gmail.com>
2021-07-07 06:30:52 +0300

    API View function

--- kittygram/urls.py
+++ kittygram/urls.py
@@ -1,9 +1,10 @@
-from django.urls import include, path
+from django.urls import path
 
-from cats.views import cat_list
+from cats.views import cat_list, cat_list_detail
 
 urlpatterns = [
    path('cats/', cat_list),
+   path('cats/<int:pk>/', cat_list_detail),
 ]
 
 
